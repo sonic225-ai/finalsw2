08fc7aaf2cd04ee95b390ef227826d285d17b31e
aida529 <aida.bekenova@narxoz.kz>
2025-12-26 18:55:34 +0500

    Initial commit

---
+++ src/main/java/swfinal/sw/SwApplication.java
@@ -0,0 +1,13 @@
+package swfinal.sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwApplication.class, args);
+	}
+
+}
